318b14748a62df321ccc697dcc8c42fe90270fe8
Jordan Smalls <smalls.jordan@gmail.com>
2025-12-18 09:03:03 -0500

    feat: init express API

---
+++ server/src/config/config.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+const corsOptions = {
+    //TODO: configure cors
+}
+
+const config = {
+    port: process.env.PORT || 4000,
+    jwt_secret: process.env.JWT_SECRET,
+    mongo_uri: process.env.MONGO_URI,
+    node_env: process.env.NODE_ENV,
+    cors_options: corsOptions
+}
+
+export default config;
