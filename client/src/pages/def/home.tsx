a1d69fb388c07759ee641704082d8ecf9b962241
Jordan Smalls <smalls.jordan@gmail.com>
2025-12-27 20:22:27 -0500

    fix: updated logic for home component - going to redirect to landing page in the future

--- client/src/pages/def/home.tsx
+++ client/src/pages/def/home.tsx
@@ -1,20 +1,34 @@
-import { Button } from "../../components/ui/button";
-import { Link } from "react-router-dom";
-import { ModeToggle } from "../../components/theme/mode-toggle";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Spinner } from "../../components/ui/spinner";
+
+// TODO: update URL for redirction to landing page
 
 const Home = () => {
+    const { userInfo } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!userInfo) {
+            window.location.href = "https://letterboxd.com/film/interstellar/";
+        } else {
+            const timer = setTimeout(() => {
+                navigate("/dashboard");
+            }, 500);
+
+            return () => clearTimeout(timer);
+        }
+    }, [userInfo, navigate]);
+
     return (
-        <>
-            <h1>Home</h1>
-            <div>
-                <Link to={"/login"}><Button>Login</Button></Link>
-                <Link to={"/signup"}><Button>Signup</Button></Link>
+        <div className="flex h-screen w-screen items-center justify-center">
+            <div className="flex flex-col items-center gap-2 text-3xl justify-center">
+                <Spinner className="size-10" />
+                <h1>Loading</h1>
             </div>
-            <div>
-                <ModeToggle />
-            </div>
-        </>
-     );
+        </div>
+    );
 }
 
 export default Home;
